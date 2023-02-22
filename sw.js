c6856ba032805c3add9087fcbcb430c37169f9cb
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-02-23 05:42:28 +0700

    Fix Screen

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-41281654d8b883b8b7a8.js"
+    "url": "webpack-runtime-393684ef1309fb612f94.js"
   },
   {
     "url": "framework-9940e29dcdb35e42cf1a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6d6a70267325c373579705a249545d46"
+    "revision": "1d0043542f0c6ad38904f46be65cb7e3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-38a1858722f0f521dbcf.js"
