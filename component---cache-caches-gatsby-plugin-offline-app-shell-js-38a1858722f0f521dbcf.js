519b22f311e80de87391f07e627b3efbf70c2b15
Mon Aekarin <Aekarin.Ongart@gmail.com>
2023-12-25 00:15:02 +0700

    remove

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-38a1858722f0f521dbcf.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[306],{1930:function(e,t,n){"use strict";var u=n(5318);t.__esModule=!0,t.default=void 0;var o=u(n(5354)),r=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-38a1858722f0f521dbcf.js.map
